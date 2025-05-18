db812f29e0f613128c235b6ef78b3f590d539ce7
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2025-05-18 10:11:55 +0000

    v1.1

---
+++ template.ino
@@ -0,0 +1,89 @@
+#include "M5AtomS3.h"
+#include <WiFi.h>
+#include <SkaarhojPgmspace.h>
+#include <ATEMbase.h>
+#include <ATEMstd.h>
+
+IPAddress switcherIp({ip_octet1}, {ip_octet2}, {ip_octet3}, {ip_octet4});	      // IP address of the ATEM switcher
+ATEMstd AtemSwitcher;
+
+const char* ssid = {ssid};
+const char* password =  {pass};
+
+int cameraNumber = 3;
+int ledPin = 10;
+
+int PreviewTallyPrevious = 1;
+int ProgramTallyPrevious = 1;
+
+void setup() {
+
+  auto cfg = M5.config();
+  AtomS3.begin(cfg);
+  AtomS3.Display.setTextColor(GREEN);
+  AtomS3.Display.setTextDatum(middle_center);
+  AtomS3.Display.setFont(&fonts::Orbitron_Light_24);
+  AtomS3.Display.setTextSize(1);
+  AtomS3.Display.clear();
+  AtomS3.Display.setTextColor(TFT_BLUE);
+  AtomS3.Lcd.fillScreen(TFT_BLACK);
+  AtomS3.Display.drawString("Starting...", AtomS3.Display.width() / 2, AtomS3.Display.height() / 2.5);
+  AtomS3.Display.setTextSize(0.5);
+  AtomS3.Display.setTextColor(TFT_RED);
+  AtomS3.Display.drawString("Powered by", AtomS3.Display.width() / 2, AtomS3.Display.height() / 1.25);
+  AtomS3.Display.setTextSize(0.7);
+  AtomS3.Display.drawString("edybalasa", AtomS3.Display.width() / 2, AtomS3.Display.height() / 1.1);;
+
+
+  Serial.begin(9600);
+
+  // Start the Ethernet, Serial (debugging) and UDP:
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+
+  delay(500);
+
+  // Initialize a connection to the switcher:
+  AtemSwitcher.begin(switcherIp);
+  AtemSwitcher.serialOutput(0x80);
+  AtemSwitcher.connect();
+}
+
+int ProgramTally;
+int PreviewTally;
+
+void loop() {
+
+  // Check for packets, respond to them etc. Keeping the connection alive!
+  AtemSwitcher.runLoop();
+
+  ProgramTally = AtemSwitcher.getProgramTally(cameraNumber);
+  PreviewTally = AtemSwitcher.getPreviewTally(cameraNumber);
+
+  if ((ProgramTallyPrevious != ProgramTally) || (PreviewTallyPrevious != PreviewTally)) { // changed?
+
+    if ((ProgramTally && !PreviewTally) || (ProgramTally && PreviewTally) ) { // only program, or program AND preview
+      AtomS3.Display.clear();
+      AtomS3.Display.setTextColor(TFT_BLACK);
+      AtomS3.Lcd.fillScreen(TFT_RED);
+      AtomS3.Display.drawString("3", AtomS3.Display.width() / 2, AtomS3.Display.height() / 2);
+    } else if (PreviewTally && !ProgramTally) { // only preview
+      AtomS3.Display.clear();
+      AtomS3.Display.setTextColor(TFT_BLACK);
+      AtomS3.Lcd.fillScreen(TFT_GREEN);
+      AtomS3.Display.drawString("3", AtomS3.Display.width() / 2, AtomS3.Display.height() / 2);
+    } else if (!PreviewTally && !ProgramTally) { // neither
+      AtomS3.Display.clear();
+      AtomS3.Display.setTextColor(TFT_WHITE);
+      AtomS3.Lcd.fillScreen(TFT_BLACK);
+      AtomS3.Display.drawString("3", AtomS3.Display.width() / 2, AtomS3.Display.height() / 2);
+    }
+  }
+  ProgramTallyPrevious = ProgramTally;
+  PreviewTallyPrevious = PreviewTally;
+}
